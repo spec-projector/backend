e47e13b8ad249521d6af11b12d02cfd976dffce2
Stanislav Lapshin <staslapshin88@gmail.com>
2019-12-02 17:16:52 +0300

    [#] Refactor me tests

--- tests/test_users/test_ghl/test_me_user.py
+++ tests/test_users/test_ghl/test_me_user.py
@@ -1,11 +1,29 @@
 # -*- coding: utf-8 -*-
 
-from apps.core.utils.objects import dict2obj
 from apps.users.graphql.resolvers import resolve_me_user
 
+GHL_QUERY_ME = """
+query {
+    me {
+        id
+      	login
+    }
+}
+"""
 
-def test_me_user(user, client):
-    client.user = user
-    info = dict2obj({'context': client})
 
-    assert resolve_me_user(None, info) == user
+def test_query(user, ghl_client):
+    """Test me query."""
+    ghl_client.set_user(user)
+
+    result = ghl_client.execute(GHL_QUERY_ME)
+
+    assert 'errors' not in result
+    assert result['data']['me']['id'] == str(user.id)
+
+
+def test_resolver(user, ghl_auth_mock_info):
+    """Test me resolver."""
+    resolved_user = resolve_me_user(None, ghl_auth_mock_info)
+
+    assert resolved_user == user
