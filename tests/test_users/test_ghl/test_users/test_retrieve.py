168bce9e79df6cc2c6a44263a428845f7fedd143
Stanislav Lapshin <staslapshin88@gmail.com>
2019-12-02 17:33:56 +0300

    [#] Refactor retrieve user tests

---
+++ tests/test_users/test_ghl/test_users/test_retrieve.py
@@ -0,0 +1,37 @@
+# -*- coding: utf-8 -*-
+
+from apps.users.graphql.types.user import UserType
+
+GHL_QUERY_USER = """
+query {{
+  user(id: {0}) {{
+    id
+    login
+  }}
+}}
+"""
+
+
+def test_query(user, ghl_client):
+    """Test getting user query."""
+    ghl_client.set_user(user)
+
+    result = ghl_client.execute(GHL_QUERY_USER.format(user.id))
+
+    assert 'errors' not in result
+    assert result['data']['user']['id'] == str(user.id)
+
+
+def test_success(user, ghl_auth_mock_info):
+    retrieved_user = UserType().get_node(ghl_auth_mock_info, user.id)
+
+    assert retrieved_user == user
+
+
+def test_inactive(user, ghl_auth_mock_info):
+    user.is_active = False
+    user.save(update_fields=['is_active'])
+
+    retrieved_user = UserType().get_node(ghl_auth_mock_info, user.id)
+
+    assert retrieved_user is None
