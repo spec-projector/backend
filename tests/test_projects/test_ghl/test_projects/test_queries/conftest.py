d4c42c90c4e47d24f4f8632af833a2e6639e42a1
Stanislav Lapshin <staslapshin88@gmail.com>
2020-03-05 12:27:45 +0300

    [#] Broke on warns in tests

--- tests/test_projects/test_ghl/test_projects/test_queries/conftest.py
+++ tests/test_projects/test_ghl/test_projects/test_queries/conftest.py
@@ -5,9 +5,11 @@ import pytest
 
 @pytest.fixture(scope="session")
 def project_query(ghl_queries):
+    """Provides project graphql query."""
     return ghl_queries.fields["project"].resolver
 
 
 @pytest.fixture(scope="session")
 def all_projects_query(ghl_queries):
+    """Provides all projects graphql query."""
     return ghl_queries.fields["allProjects"].resolver
