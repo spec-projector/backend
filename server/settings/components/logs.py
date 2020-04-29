0d5c7ad3b3bf1a93427f6ead406101f48092d216
Stanislav Lapshin <staslapshin88@gmail.com>
2020-04-29 16:16:43 +0300

    [#] Add sentry

--- server/settings/components/logs.py
+++ server/settings/components/logs.py
@@ -1,5 +1,9 @@
 # -*- coding: utf-8 -*-
 
+import sentry_sdk
+from decouple import config
+from sentry_sdk.integrations.django import DjangoIntegration
+
 STANDARD_FORMAT = "[%(asctime)s]|%(levelname)s|%(module)s.%(funcName)s:%(lineno)s|%(message)s"  # noqa: E501
 
 LOGGING = {
@@ -30,3 +34,11 @@ LOGGING = {
         },
     },
 }
+
+sentry_dsn = config("DJANGO_SENTRY_DSN", default=None)
+if sentry_dsn:
+    sentry_sdk.init(  # type:ignore
+        dsn=sentry_dsn,
+        integrations=[DjangoIntegration()],
+        send_default_pii=True,
+    )
