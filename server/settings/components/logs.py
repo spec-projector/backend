788bd5498a0eb9b364083ce5fd0461edc5ff42ea
Stanislav Lapshin <sm.lapshin@gmail.com>
2020-09-23 12:13:57 +0300

    [#59] Suppress graphql normal errors into sentry

--- server/settings/components/logs.py
+++ server/settings/components/logs.py
@@ -42,20 +42,26 @@ LOGGING = {
 IGNORED_ERRORS_TXT = ("graphql.error.located_error.GraphQLLocatedError",)
 
 
-def _before_send_sentry_handler(event, hint):
-    exc_info = hint.get("exc_info")
-    if exc_info:
-        _, exc_value, tb = exc_info
-        if isinstance(exc_value, GraphQLError):
-            return None
-    else:
-        log_record = hint.get("log_record")
-        skip_event = log_record and any(
-            err in log_record.message for err in IGNORED_ERRORS_TXT
+class _BeforeSendHandler:
+    def __call__(self, event, hint):
+        """Filter by exception."""
+        exc_info = hint.get("exc_info")
+
+        skip_event = (
+            exc_info and self._skip_by_error(exc_info)
+        ) or self._skip_by_message(event)
+
+        if not skip_event:
+            return event
+
+    def _skip_by_error(self, exc_info) -> bool:
+        exc_type, exc_value, tb = exc_info
+        return isinstance(exc_value, GraphQLError)
+
+    def _skip_by_message(self, event) -> bool:
+        return any(
+            err in event["logentry"]["message"] for err in IGNORED_ERRORS_TXT
         )
-        if skip_event:
-            return None
-    return event
 
 
 sentry_dsn = config("DJANGO_SENTRY_DSN", default=None)
@@ -65,7 +71,6 @@ if sentry_dsn:
         release=SP_APP_VERSION,
         integrations=[DjangoIntegration()],
         send_default_pii=True,
-        before_send=_before_send_sentry_handler,
-        ignore_errors=[GraphQLError],
+        before_send=_BeforeSendHandler(),
     )
     sentry_sdk.utils.MAX_STRING_LENGTH = 4096
