d65bb8b2b6fb41b8f0503678b179f9659c7999df
Stanislav Lapshin <sm.lapshin@gmail.com>
2021-02-23 00:02:51 +0300

    Fix broken quality

--- server/apps/users/logic/use_cases/auth/login.py
+++ server/apps/users/logic/use_cases/auth/login.py
@@ -9,7 +9,7 @@ from apps.core.logic.errors import BaseApplicationError
 from apps.core.logic.use_cases import BaseUseCase
 from apps.core.services.errors import BaseInfrastructureError
 from apps.users.logic.interfaces import ILoginService, ITokenService
-from apps.users.models import Token
+from apps.users.models import Token, User
 
 
 @dataclass(frozen=True)
@@ -56,7 +56,6 @@ class UseCase(BaseUseCase):
         self._validate_input(input_dto)
 
         try:
-
             user = self._login_service.login(
                 input_dto.username,
                 input_dto.password,
@@ -64,14 +63,16 @@ class UseCase(BaseUseCase):
         except BaseInfrastructureError as err:
             raise LoginError(err.code, str(err))
         else:
-            user.last_login = timezone.now()
-            user.save(update_fields=("last_login",))
+            self._update_user(user)
 
-            token = self._token_service.create_user_token(user)
             return OutputDto(
-                token=token,
+                token=self._token_service.create_user_token(user),
             )
 
     def _validate_input(self, input_dto: InputDto) -> None:
         if not input_dto.username or not input_dto.password:
             raise EmptyCredentialsError()
+
+    def _update_user(self, user: User):
+        user.last_login = timezone.now()
+        user.save(update_fields=("last_login",))
