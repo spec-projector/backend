bd9629c2fe8400f9efcf27800b7fef5c410aa096
Andrey Surovov <andrey.surovov@gmail.com>
2021-02-25 16:35:53 +0300

    Merge branch 'master' into #89

--- server/apps/core/admin/base/model_admin.py
+++ server/apps/core/admin/base/model_admin.py
@@ -1,5 +1,5 @@
 from django.contrib import admin
-from jnt_admin_tools.mixins.autocomplete import AutocompleteAdminMixin
+from jnt_admin_tools.mixins import AutocompleteAdminMixin
 
 from apps.core.admin.mixins import AdminFormFieldsOverridesMixin
 
