3b3cce494ab10ba1b66b064ee120a52b573b9c51
Stanislav Lapshin <sm.lapshin@gmail.com>
2021-04-08 17:15:20 +0300

    Refactor projects graphql query

--- server/apps/projects/graphql/fields/all_projects.py
+++ server/apps/projects/graphql/fields/all_projects.py
@@ -1,28 +1,36 @@
 import graphene
-from jnt_django_graphene_toolbox.fields import BaseModelConnectionField
-from jnt_django_graphene_toolbox.filters import SortHandler
+from django.db import models
+from graphql import ResolveInfo
 
-from apps.projects.graphql.fields.filters import ProjectsFilterSet
+from apps.core.graphql.fields import BaseQueryConnectionField
 from apps.projects.graphql.types import ProjectType
+from apps.projects.logic.queries.project import allowed
 
 
-class ProjectSort(graphene.Enum):
-    """Allowed sort fields."""
-
-    CREATED_AT_ASC = "created_at"  # noqa: WPS115
-    CREATED_AT_DESC = "-created_at"  # noqa: WPS115
-
-
-class ProjectConnectionField(BaseModelConnectionField):
+class ProjectConnectionField(BaseQueryConnectionField):
     """Handler for projects collections."""
 
-    filterset_class = ProjectsFilterSet
+    query = allowed.Query
     auth_required = True
-    sort_handler = SortHandler(ProjectSort)
 
     def __init__(self):
         """Initialize."""
         super().__init__(
             ProjectType,
             title=graphene.String(),
+        )
+
+    @classmethod
+    def get_input_dto(
+        cls,
+        queryset: models.QuerySet,
+        info: ResolveInfo,  # noqa: WPS110
+        args,
+    ):
+        """Prepare query input data."""
+        return allowed.InputDto(
+            queryset=queryset,
+            filters=cls.get_filters_from_args(args, allowed.ProjectFilter),
+            sort=cls.get_sort_from_args(args),
+            user=info.context.user,
         )
