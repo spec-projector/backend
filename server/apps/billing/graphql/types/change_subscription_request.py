cedf0fe8131b741b137d4e3825a199d3d11379e5
Stanislav Lapshin <sm.lapshin@gmail.com>
2021-04-09 18:18:10 +0300

    Add from_subscription, to_subscription to ChangeSubscriptionRequestType

--- server/apps/billing/graphql/types/change_subscription_request.py
+++ server/apps/billing/graphql/types/change_subscription_request.py
@@ -1,9 +1,8 @@
 import graphene
 from jnt_django_graphene_toolbox.types import BaseModelObjectType
 
-from apps.billing.graphql.types import TariffType
+from apps.billing.graphql.types import SubscriptionType, TariffType
 from apps.billing.models import ChangeSubscriptionRequest
-from apps.billing.models.enums import SubscriptionStatus
 
 
 class ChangeSubscriptionRequestType(BaseModelObjectType):
@@ -16,4 +15,5 @@ class ChangeSubscriptionRequestType(BaseModelObjectType):
     created_at = graphene.DateTime(required=True)
     is_active = graphene.Boolean(required=True)
     tariff = graphene.Field(TariffType, required=True)
-    status = graphene.Enum.from_enum(SubscriptionStatus)(required=True)
+    from_subscription = graphene.Field(SubscriptionType)
+    to_subscription = graphene.Field(SubscriptionType)
