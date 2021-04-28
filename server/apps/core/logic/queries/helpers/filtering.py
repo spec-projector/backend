a02937d0f3baf49157b6a6e047bc4871ca444a90
Stanislav Lapshin <sm.lapshin@gmail.com>
2021-04-28 14:53:52 +0300

    Fix broken lints

--- server/apps/core/logic/queries/helpers/filtering.py
+++ server/apps/core/logic/queries/helpers/filtering.py
@@ -17,17 +17,21 @@ def filter_queryset(
     if not filters:
         return queryset
 
-    filters = {
-        data_key: data_value
-        for data_key, data_value in dataclasses.asdict(filters).items()
-        if data_value != empty
-    }
+    prepared_filters = _prepare_filters(filters)
 
-    if not filters:
+    if not prepared_filters:
         return queryset
 
-    filterset = filterset_class(data=filters, queryset=queryset)
+    filterset = filterset_class(data=prepared_filters, queryset=queryset)
     if not filterset.is_valid():
         raise InvalidInputApplicationError(filterset.errors)
 
     return filterset.qs
+
+
+def _prepare_filters(filters) -> ty.Dict[str, object]:
+    return {
+        data_key: data_value
+        for data_key, data_value in dataclasses.asdict(filters).items()
+        if data_value != empty
+    }
