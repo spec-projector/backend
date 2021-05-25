f3cbe8e5b64da18b4e925cc40d150d59b2a686e7
Andrey Surovov <andrey.surovov@gmail.com>
2021-05-25 14:47:17 +0300

    Refactor cleanup as service #130

--- server/apps/core/models/base.py
+++ server/apps/core/models/base.py
@@ -1,8 +1,6 @@
 from django.db import models
 from django.dispatch import receiver
 
-from apps.core.utils.media import cleanup_media_files
-
 
 class BaseModel(models.Model):
     """Base model."""
@@ -14,7 +12,13 @@ class BaseModel(models.Model):
 @receiver(models.signals.post_delete)
 def post_delete_base_model_handler(sender, instance, *args, **kwargs):
     """Base post delete handler."""
+    from apps.core import injector  # noqa: WPS433
+    from apps.media.logic.interfaces import (  # noqa: WPS433
+        ICleanupMediaFilesService,
+    )
+
     if not isinstance(instance, BaseModel):
         return
 
-    cleanup_media_files(instance)
+    cleanup_service = injector.get(ICleanupMediaFilesService)
+    cleanup_service.cleanup_media_files(instance)
--- server/apps/media/services/__init__.py
+++ server/apps/media/services/__init__.py
@@ -1,1 +1,2 @@
+from .cleanup_media_files import CleanupMediaFilesService
 from .image import ImageDownloadService
