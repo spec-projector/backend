8c40ec375bc51f9f76ef698eb5b38c5fa5b0728a
Stanislav Lapshin <sm.lapshin@gmail.com>
2021-06-14 14:52:25 +0300

    Add docker-compose

--- develop/docker.sh
+++ develop/docker.sh
@@ -1,6 +1,5 @@
 #!/usr/bin/env sh
 
-docker-compose -p sp \
-               -f develop/docker-compose.base.yml \
+docker-compose -f develop/docker-compose.base.yml \
                -f develop/docker-compose.docker.yml \
                up -d --remove-orphans
--- develop/local.sh
+++ develop/local.sh
@@ -1,5 +1,4 @@
 #!/usr/bin/env sh
 
-docker-compose -p sp \
-               -f develop/docker-compose.base.yml \
+docker-compose -f develop/docker-compose.base.yml \
                up -d --remove-orphans
