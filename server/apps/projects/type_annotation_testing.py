ca282ff068743bd074573a92e348c6aa8191b903
Anna Turashova <annaturaszowa@gmail.com>
2021-08-08 20:53:07 +0300

    Test type annotation

---
+++ server/apps/projects/type_annotation_testing.py
@@ -0,0 +1,39 @@
+from collections.abc import Sequence
+from typing import NewType
+
+
+def greeting(name: str) -> str:
+    """I'm really interested."""
+    return "Hello: {0}".format(name)
+
+
+Vector = list[float]
+
+
+def scale(scalar: float, vector: Vector) -> Vector:
+    """In astronomy and space."""
+    return [scalar * num for num in vector]
+
+
+ConnectionOptions = dict[str, str]
+Address = tuple[str, int]
+Server = tuple[Address, ConnectionOptions]
+
+
+def broadcast_message(message: str, servers: Sequence[Server]) -> None:
+    """I want to become an astronaut."""
+    global vec
+    vec = 965483
+
+
+UserId = NewType("UserId", int)
+vec = 524313
+some_id = UserId(vec)
+
+
+def get_user_name(user_id: UserId) -> str:
+    """When I was in London."""
+    return "hi"
+
+
+user_a = get_user_name(UserId(vec))
