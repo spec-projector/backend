549f8432af5eb9cd771a24aee8f2105c578003b1
Anna Turashova <annaturaszowa@gmail.com>
2021-08-09 23:42:14 +0300

    Test type annotation

--- server/apps/projects/type_annotation_testing.py
+++ server/apps/projects/type_annotation_testing.py
@@ -1,40 +1,12 @@
-from typing import Dict, List, NewType, Tuple
+from typing import Dict, List, Optional
+
+list_str = List[str]
+dict_str = Dict[str, str]
 
 
-def greeting(name: str) -> str:
-    """I'm really interested."""
-    return "Hello: {0}".format(name)
-
-
-Vector = List[float]
-
-
-def scale(scalar: float, vector: Vector) -> Vector:
-    """In astronomy and space."""
-    return [scalar * num for num in vector]
-
-
-ConnectionOptions = Dict[str, str]
-Address = Tuple[str, int]
-Server = Tuple[Address, ConnectionOptions]
-
-
-def broadcast_message(message: str) -> None or str:
+def broadcast_message() -> Optional[str]:
     """I want to become an astronaut."""
     tea = None
     if tea is None:
         tea = "sweet tea"
     return tea
-
-
-UserId = NewType("UserId", int)
-vec = 524313
-some_id = UserId(vec)
-
-
-def get_user_name(user_id: UserId) -> str:
-    """When I was in London."""
-    return "hi"
-
-
-user_a = get_user_name(UserId(vec))
